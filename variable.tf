e74b50c4bfe677312210fe440d9a1ee8c6e9a827
Manas Dubey <95405919+Dubeman@users.noreply.github.com>
2025-02-11 00:15:06 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -16,3 +16,16 @@ variable "aws_region" {
   description = "The AWS region to deploy resources in"
   default     = "us-east-1"
 }
+
+
+variable "aws_access_key_id" {
+  description = "The AWS access key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "The AWS secret access key"
+  type        = string
+  sensitive   = true
+}
