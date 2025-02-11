23260f88ea1ea0f299dc563e710288b6c2b67f25
Manas Dubey <95405919+Dubeman@users.noreply.github.com>
2025-02-10 23:52:53 -0500

    Updated variable.tf with app env and region

--- variable.tf
+++ variable.tf
@@ -4,3 +4,15 @@ variable "instance_type" {
   description = "Instance type for the EC2 instance"
   default     = "t2.micro"  # Default to t2.micro if no value is provided
 }
+
+variable "app_environment" {
+  type        = string
+  description = "Type of environment"
+  default     = "dev"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS region to deploy resources in"
+  default     = "us-east-1"
+}
